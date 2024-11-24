198776aa1247fdf42ce959141cb326841fd75a66
Vince <2330047@bart.ca>
2024-11-23 22:42:15 -0500

    implémentation des visuels du magasin, de l'achat d'upgrades et chargement d'upgrades. basics du jeu / clicker implémenté.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const request = indexedDB.open("MainDatabase", 1)
+let clicker_extra = 1
+let clicker_multiplier = 1
+
+let clicker_MoneyGained = 0
+
+let db
+const request = indexedDB.open("MainDatabase", 1);
 
 request.onupgradeneeded = function(event) {
     db = event.target.result
-    objectStore = db.createObjectStore("MainObjectStore", {keyPath: "username"})
+    const objectStore = db.createObjectStore("MainObjectStore", {keyPath: "username"})
     objectStore.createIndex("currency", "currency", { unique: false })
 }
 
+request.onsuccess = function(event) {
+    db = event.target.result
+}
+
 function addData(Account) {
-    db = request.result
     transaction = db.transaction(["MainObjectStore"], "readwrite")
     objectStore = transaction.objectStore("MainObjectStore")
     
@@ -22,7 +31,6 @@ function addData(Account) {
 
 function getData(username) {
     return new Promise((resolve) => {
-        db = request.result
         transaction = db.transaction(["MainObjectStore"], "readonly")
         objectStore = transaction.objectStore("MainObjectStore")
         
@@ -38,7 +46,6 @@ function getData(username) {
 
 function updateData(_username, _password, _currency, _oneTimeUpgrades, _normalUpgrades) { // new data must be in this form: {username: "keer", password: "aaa", currency: 1000}
     newData = { username: _username, password: _password, currency: _currency, oneTimeUpgrades: _oneTimeUpgrades, normalUpgrades: _normalUpgrades }
-    db = request.result
     transaction = db.transaction(["MainObjectStore"], "readwrite")
     objectStore = transaction.objectStore("MainObjectStore")
     updateRequest = objectStore.put(newData)
@@ -66,7 +73,6 @@ function logData(username) { // To be called in the console
 }
 
 function logKeys() { // To be called in the console
-    db = request.result
     transaction = db.transaction(["MainObjectStore"], "readwrite")
     objectStore = transaction.objectStore("MainObjectStore")
     console.log(objectStore.getAllKeys())
@@ -74,7 +80,6 @@ function logKeys() { // To be called in the console
 }
 
 function resetData() { // To be called in the console
-    db = request.result
     transaction = db.transaction(["MainObjectStore"], "readwrite")
     objectStore = transaction.objectStore("MainObjectStore")
     clearRequest = objectStore.clear()
@@ -93,7 +98,12 @@ class Account {
     }
 }
 
-
+class OneTimeUpgrade {
+    constructor(upgradeName, value) {
+        this.name = upgradeName
+        this.value = value
+    }
+}
 
 
 
@@ -154,6 +164,9 @@ function checkUsernameTaken() {
 }
 
 function createAccount() {
+    if (document.getElementById("usernameError").style.display == "flex") {
+        return
+    }
     let usernameElement = document.getElementById("username").value
     let passwordElement = document.getElementById("password").value
     if (usernameElement != "" && passwordElement != "") {
@@ -164,6 +177,8 @@ function createAccount() {
     }
     acc = new Account(username, password, 0, [false, false, false], [0, 0, 0])
     addData(acc)
+    document.getElementById("createAccountForm").remove()
+    document.getElementById("CreatedAccountPrompt").style.display = "grid"
 }
 
 async function logInAccount() {
@@ -176,11 +191,19 @@ async function logInAccount() {
     if (passwordElement == acc.password) {
         console.log(usernameElement) // LOGIN HERE
         setLoginCookie("login", usernameElement, 1)
+        window.location.href='index.html'
     } else {
         passwordElement = document.getElementById("password").value = ""
         alert("Username or Password is incorrect!")
     }
     
+}
+
+function logOutAccount() {
+    if (confirm("Do you want to log out of " + getLoginCookie("login") + "?")) {
+        setLoginCookie("login", "", 0)
+        location.reload()
+    }
 }
 
 async function getAccount(username) {
@@ -202,26 +225,174 @@ async function test(user) {
     let acc = await getAccount(user)
     console.log(acc.username)
     console.log(acc.password)
-    console.log(acc.oneTimeUpgrades[0])
-    console.log(acc.oneTimeUpgrades[1])
-    console.log(acc.oneTimeUpgrades[2])
-    return user
+    
+    for (let i = 0; i < acc.oneTimeUpgrades.length; i++) {
+        console.log(acc.oneTimeUpgrades[i])
+    }
+
+    for (let i = 0; i < acc.normalUpgrades.length; i++) {
+        console.log(acc.normalUpgrades[i])
+    }
+
 }
 
-window.onload = function() {
+async function BuyUpgrade(cost, upgradeType, index) {
+    let username = getLoginCookie("login")
+    let acc = await getAccount(username)
+    if ((acc.currency - cost) >= 0) {
+
+        let updatedCurrency = acc.currency - cost
+        let updatedOneTimeUpgrades = [false, false, false]
+        let updatedNormalUpgrades = [0, 0, 0]
+
+        for (let i = 0; i < acc.oneTimeUpgrades.length; i++) {
+            updatedOneTimeUpgrades[i] = acc.oneTimeUpgrades[i]
+        }
+
+        for (let i = 0; i < acc.normalUpgrades.length; i++) {
+            updatedNormalUpgrades[i] = acc.normalUpgrades[i]
+        }
+
+        switch (upgradeType) {
+            case "oneTime":
+                updatedOneTimeUpgrades[index] = true
+                break;
+            case "normal":
+                updatedNormalUpgrades[index] += 1
+                break;
+            default:
+                console.error("upgradeType WAS MISPELLED IN HTML, ERROR COMING FROM BuyUpgrade()")
+                break;
+        }
+
+        updateData(acc.username, acc.password, updatedCurrency, updatedOneTimeUpgrades, updatedNormalUpgrades)
+        location.reload()
+    } else {
+        if (username == "") {
+            alert("You require an account to perform this action!")
+        } else {
+            alert("Insufficient funds!")
+        }
+    }
+}
+
+
+
+async function mineClicked() {
+    let username = getLoginCookie("login")
+    let userCurrency = document.getElementById("userCurrency")
+    let acc = await getAccount(username)
+    clicker_MoneyGained += (clicker_extra * clicker_multiplier)
+    userCurrency.textContent = "$" + String(acc.currency + clicker_MoneyGained)
+}
+
+async function saveMoney() {
+    let username = getLoginCookie("login")
+    let acc = await getAccount(username)
+    let updatedMoney = acc.currency + clicker_MoneyGained
+    updateData(acc.username, acc.password, updatedMoney, acc.oneTimeUpgrades, acc.normalUpgrades)
+}
+
+
+
+async function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function showCurrency(user) {
+    try {
+        await wait(20)
+
+        let userCurrency = document.getElementById("userCurrency")
+        let acc = await getAccount(user)
+        
+        userCurrency.textContent = "$" + String(acc.currency)
+    } catch (error) {
+        showCurrency(user)
+    }
+}
+
+async function showUpgrades(user) {
+    try {
+        await wait(20)
+
+        let acc = await getAccount(user)
+
+        for (let i = 0; i < acc.oneTimeUpgrades.length; i++) {
+            if (acc.oneTimeUpgrades[i] == true) {
+                document.getElementById("oneTimeUpgrade" + String(i)).children[4].style.display = "flex"
+            }
+        }
+
+        for (let i = 0; i < acc.normalUpgrades.length; i++) {
+            document.getElementById("normal" + String(i)).children[4].textContent = "Amount: " + String(acc.normalUpgrades[i])
+        }
+        
+    } catch (error) {
+        showUpgrades(user)
+    }
+}
+
+async function updateClickerValues() {
+    try {
+        await wait(20)
+
+        let username = getLoginCookie("login")
+        let acc = await getAccount(username)
+
+        if (acc.oneTimeUpgrades[0] == true) {   // Copper Deposit
+            clicker_multiplier += 1             // ( x2 - 1 )
+        }
+        if (acc.oneTimeUpgrades[1] == true) {   // Iron Deposit
+            clicker_multiplier += 3             // ( x4 - 1 )
+        }
+        if (acc.oneTimeUpgrades[1] == true) {   // Diamond Deposit
+            clicker_multiplier += 7             // ( x8 - 1 )
+        }
+        
+        clicker_extra += acc.normalUpgrades[0] * 40 // Extra Mineshaft
+        clicker_extra += acc.normalUpgrades[1] * 1 // Extra Strength
+        clicker_extra += acc.normalUpgrades[2] * 15 // Extra Helping Hand
+
+    } catch (error) {
+        updateClickerValues()
+    }
+}
+
+window.onload = function() { // WHEN PAGE LOADS
     let pageName = document.getElementById("pageName").textContent
     let loggedUser = document.getElementById("loggedUsername")
+    let userCurrency = document.getElementById("userCurrency")
+    let signUpButton = document.getElementById("signUpButton")
+    let logInButton = document.getElementById("logInButton")
+    let username = getLoginCookie("login")
 
-    console.log(loggedUser.textContent)
-    console.log(getLoginCookie("login"))
+    if (username != "") {
+        loggedUser.textContent = username
+        signUpButton.style.display = "none"
+        logInButton.style.display = "none"
 
-    if (getLoginCookie("login") != "") {
-        console.log("should work")
-        loggedUser.textContent = getLoginCookie("login")
+        showCurrency(username)
+
+    } else {
+        loggedUser.style.display = "none"
+        userCurrency.style.display = "none"
     }
 
     if (pageName == "signUp") {
         let usernameInput = document.getElementById("username")
         usernameInput.addEventListener("input", checkUsernameTaken)
+    }
+
+    if (pageName == "home") {
+        showUpgrades(username)
+    }
+
+    if (pageName == "game") {
+        window.addEventListener('beforeunload', function() {  
+            saveMoney()    
+        })
+
+        updateClickerValues()
     }
 }
